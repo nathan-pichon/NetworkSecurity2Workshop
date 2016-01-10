66e2ffcbc53b14ea7de937468a08b68950bfde4f
AdrienLouf <l_adrien@ymail.com>
2016-01-10 20:10:46 +0100

    create c++ project with sslib (using XCode)

---
+++ src/NetworkSecurity2Workshop/NetworkSecurity2Workshop/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  NetworkSecurity2Workshop
+//
+//  Created by adrienl on 10/01/2016.
+//  Copyright © 2016 adrienlouf. All rights reserved.
+//
+
+#include <iostream>
+#include "SSHSessionManager.hpp"
+
+int main(int argc, const char * argv[]) {
+    SSHSessionManager ns = SSHSessionManager();
+    
+    std::cout << "Hello, World!\n";
+    return 0;
+}

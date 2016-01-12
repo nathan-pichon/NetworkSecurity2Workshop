fa7719006a1da2dd89641f83550fc4e9f4faeae8
AdrienLouf <l_adrien@ymail.com>
2016-01-13 00:47:58 +0100

    add spatch project

---
+++ Spatch/Spatch/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Spatch
+//
+//  Created by adrienl on 13/01/2016.
+//  Copyright © 2016 adrienlouf. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
